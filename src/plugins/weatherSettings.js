f6818bfd95e3e585f7e7feac762d93efe57f5982
Eugene Yesaulov <enot8826@gmail.com>
2021-04-15 09:33:39 +0300

    Update weatherSettings.js
    privacy fixes

--- src/plugins/weatherSettings.js
+++ src/plugins/weatherSettings.js
@@ -1,4 +1,4 @@
-export const appid = '055c8c73f82660202fe7a436e7b76b31';
+export const appid = 'YOUR_APPID';
 export const weather_url = 'https://api.openweathermap.org/data/2.5/';
 export const weather_type = {
   weather : 'weather',
